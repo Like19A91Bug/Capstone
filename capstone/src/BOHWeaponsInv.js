ad09484dd32a8b55068d536284f0d2e464c354e6
Like19A91Bug <bugnificent.tk@gmail.com>
2023-08-23 17:45:34 -0400

    Update BOHWeaponsInv.js

--- capstone/src/BOHWeaponsInv.js
+++ capstone/src/BOHWeaponsInv.js
@@ -18,14 +18,42 @@ const BOHWeaponsInv = () => {
     return (
         <div id="weaponsInv" className="col space">
             <h3>Weapons</h3>
+            <div className="row">
+                <div className="col-3">Weapon</div>
+                <div className="col">Category Range</div>
+                <div className="col">Damage</div>
+                <div className="col">DMG Type</div>
+                <div className="col">Range</div>
+                <div className="col">lbs</div>
+                <div className="col">Properties</div>
+                <div className="col">Qty</div>
+            </div>
+            <hr />
             <h5>Simple Melee</h5>
-            <WeaponDropdown />
+            <WeaponRow weapon={weapon} editMode={editMode} />
             <h5>Martial Melee</h5>
-            <WeaponDropdown />
+            <WeaponRow weapon={weapon} editMode={editMode} />
             <h5>Simple Ranged</h5>
-            <WeaponDropdown />
+            <WeaponRow weapon={weapon} editMode={editMode} />
             <h5>Martial Ranged</h5>
-            <WeaponDropdown />
+            <WeaponRow weapon={weapon} editMode={editMode} />
+            {editMode ? (
+                <button
+                    onClick={(event) => {
+                        setEditMode(!editMode);
+                    }}
+                >
+                    Save
+                </button>
+            ) : (
+                <button
+                    onClick={(event) => {
+                        setEditMode(!editMode);
+                    }}
+                >
+                    Edit
+                </button>
+            )}
         </div>
     );
 };
@@ -74,13 +102,49 @@ const WeaponDropdown = ({
                 {weapon.map((weapon) => {
                     // console.log(weapon);
                     return (
-                        <option key={weapon.index} content={weapon.name}>
+                        <option key={weapon.index} content={weapon.url}>
                             {weapon.name}
                         </option>
                     );
                 })}
             </select>
         </div>
+    );
+};
+
+const WeaponStats = ({ selectedWeapon }) => {
+    const [weaponDetails, setWeaponDetails] = useState();
+    useEffect(() => {
+        const makeAPICall = async () => {
+            const response = await fetch(
+                `https://www.dnd5eapi.co${selectedWeapon}`
+            );
+            const data = await response.json();
+            setWeaponDetails(data);
+            console.log(data);
+        };
+        if (selectedWeapon) {
+            makeAPICall();
+        }
+    }, [selectedWeapon]);
+
+    if (!selectedWeapon) {
+        return <div>Press "Edit" and make a selection in dropdown</div>;
+    }
+    if (!weaponDetails) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <>
+            <div className="col-1">{weaponDetails.category_range}</div>
+            <div className="col-1">{weaponDetails.damage.damage_dice}</div>
+            <div className="col-1">{weaponDetails.damage.damage_dice}</div>
+            <div className="col">{weaponDetails.range.normal}</div>
+            <div className="col">{weaponDetails.weight}</div>
+            <div className="col">
+                {weaponDetails.cost.quantity} {weaponDetails.cost.unit}
+            </div>
+        </>
     );
 };
 
